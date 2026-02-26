42baa4e73660888356e5c27425f1bb135baecfd2
justin721124-cyber <justin721124@gmail.com>
2026-02-26 00:59:55 -0400

    Formulario8

---
+++ formulario8/formulario8/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace formulario8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnContar = new Button();
+            lblResultado = new Label();
+            txtOracion = new TextBox();
+            SuspendLayout();
+            // 
+            // btnContar
+            // 
+            btnContar.Location = new Point(0, 0);
+            btnContar.Name = "btnContar";
+            btnContar.Size = new Size(112, 34);
+            btnContar.TabIndex = 0;
+            btnContar.Text = "contar";
+            btnContar.UseVisualStyleBackColor = true;
+            btnContar.Click += btnContar_Click;
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Location = new Point(154, 164);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(69, 25);
+            lblResultado.TabIndex = 1;
+            lblResultado.Text = "cantida";
+            // 
+            // txtOracion
+            // 
+            txtOracion.Location = new Point(0, 205);
+            txtOracion.Multiline = true;
+            txtOracion.Name = "txtOracion";
+            txtOracion.Size = new Size(427, 32);
+            txtOracion.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(427, 305);
+            Controls.Add(txtOracion);
+            Controls.Add(lblResultado);
+            Controls.Add(btnContar);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnContar;
+        private Label lblResultado;
+        private TextBox txtOracion;
+    }
+}
---
+++ formulario8/formulario8/Form1.cs
@@ -0,0 +1,27 @@
+namespace formulario8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnContar_Click(object sender, EventArgs e)
+        {
+            string oracion = txtOracion.Text.Trim();
+
+            if (string.IsNullOrEmpty(oracion))
+            {
+                MessageBox.Show("Ingrese una oración.");
+                return;
+            }
+
+            string[] palabras = oracion.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int cantidad = palabras.Length;
+
+            lblResultado.Text = "Cantidad de palabras: " + cantidad;
+        }
+    }
+}
