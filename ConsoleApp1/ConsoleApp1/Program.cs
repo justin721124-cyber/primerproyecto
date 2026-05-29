99f073e70ce2f82a047bdf489505e791c15b8e56
justin721124-cyber <justin721124@gmail.com>
2026-05-29 00:43:05 -0400

    primer proyecto

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Variables con su tipo de datos
+
+String nombre = "Justin";
+String apellido = "Santana";
+int edad = 18;
+float altura = 5.9f;
+
+Console.WriteLine("Hola mi nombre es " + nombre + " y tengo " + edad + " años, y mi altura es " + altura);
